3d4ae6b30a1783630622c877dc7c6438613aadcc
harish1907 <nehasharma@tranktechies.com>
2024-12-03 17:03:44 +0530

    upate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./Screens/Admin/Dashboard";
 import EmployeeTab from "./Screens/Admin/EmployeeTab";
